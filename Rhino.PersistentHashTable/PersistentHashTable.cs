52036f20a3e54304b6218b951ba7cbda0b5f7882
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2009-02-18 16:55:50 +0000

    better handling of errors during initialization
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@2041 079b0acf-d9fa-0310-9935-e5ade295c882

--- Rhino.PersistentHashTable/PersistentHashTable.cs
+++ Rhino.PersistentHashTable/PersistentHashTable.cs
@@ -11,7 +11,6 @@ namespace Rhino.PersistentHashTable
         private readonly Instance instance;
         private bool needToDisposeInstance;
         private readonly string database;
-        public Action<InstanceParameters> Configure;
         private readonly string path;
 
         public Guid Id { get; private set; }
@@ -19,7 +18,7 @@ namespace Rhino.PersistentHashTable
         public PersistentHashTable(string database)
         {
             this.database = database;
-        	path = database;
+            path = database;
             if (Path.IsPathRooted(database) == false)
                 path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, database);
             this.database = Path.Combine(path, Path.GetFileName(database));
@@ -35,48 +34,49 @@ namespace Rhino.PersistentHashTable
             instance.Parameters.SystemDirectory = Path.Combine(path, "system");
             instance.Parameters.LogFileDirectory = Path.Combine(path, "logs");
 
-            if (Configure != null)
-                Configure(instance.Parameters);
-
             try
             {
                 instance.Init();
+
+                needToDisposeInstance = true;
+
+                EnsureDatabaseIsCreatedAndAttachToDatabase();
+
+                SetIdFromDb();
             }
             catch (Exception e)
             {
+                if (needToDisposeInstance)
+                    instance.Dispose();
+                needToDisposeInstance = false;
                 throw new InvalidOperationException("Could not open cache: " + database, e);
             }
-            needToDisposeInstance = true;
-
-            EnsureDatabaseIsCreatedAndAttachToDatabase();
-
-            SetIdFromDb();
         }
 
         private void SetIdFromDb()
         {
-        	try
-        	{
-        		instance.WithDatabase(database, (session, dbid) =>
-        		{
-        			using (var details = new Table(session, dbid, "details", OpenTableGrbit.ReadOnly))
-        			{
-        				Api.JetMove(session, details, JET_Move.First, MoveGrbit.None);
-        				var columnids = Api.GetColumnDictionary(session, details);
-        				var column = Api.RetrieveColumn(session, details, columnids["id"]);
-        				Id = new Guid(column);
-        				var schemaVersion = Api.RetrieveColumnAsString(session, details, columnids["schema_version"]);
-        				if(schemaVersion!=SchemaCreator.SchemaVersion)
-        					throw new InvalidOperationException("The version on disk ("+schemaVersion+") is different that the version supported by this library: "+SchemaCreator.SchemaVersion + Environment.NewLine+
-        					                                    "You need to migrate the disk version to the library version, alternatively, if the data isn't important, you can delete the file and it will be re-created (with no data) with the library version.");
-        			}
-        		});
-        	}
-        	catch (Exception e)
-        	{
-        		throw new InvalidOperationException("Could not read db details from disk. It is likely that there is a version difference between the library and the db on the disk." + Environment.NewLine +
-				"You need to migrate the disk version to the library version, alternatively, if the data isn't important, you can delete the file and it will be re-created (with no data) with the library version.", e);
-        	}
+            try
+            {
+                instance.WithDatabase(database, (session, dbid) =>
+                {
+                    using (var details = new Table(session, dbid, "details", OpenTableGrbit.ReadOnly))
+                    {
+                        Api.JetMove(session, details, JET_Move.First, MoveGrbit.None);
+                        var columnids = Api.GetColumnDictionary(session, details);
+                        var column = Api.RetrieveColumn(session, details, columnids["id"]);
+                        Id = new Guid(column);
+                        var schemaVersion = Api.RetrieveColumnAsString(session, details, columnids["schema_version"]);
+                        if (schemaVersion != SchemaCreator.SchemaVersion)
+                            throw new InvalidOperationException("The version on disk (" + schemaVersion + ") is different that the version supported by this library: " + SchemaCreator.SchemaVersion + Environment.NewLine +
+                                                                "You need to migrate the disk version to the library version, alternatively, if the data isn't important, you can delete the file and it will be re-created (with no data) with the library version.");
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Could not read db details from disk. It is likely that there is a version difference between the library and the db on the disk." + Environment.NewLine +
+                "You need to migrate the disk version to the library version, alternatively, if the data isn't important, you can delete the file and it will be re-created (with no data) with the library version.", e);
+            }
         }
 
         private void EnsureDatabaseIsCreatedAndAttachToDatabase()
